b2fcca88e8925f7c74cfae944dfd6992b38cd611
KYK <ken19505@gmail.com>
2024-09-09 16:10:58 +0800

    can plot the required now

--- plot_map.py
+++ plot_map.py
@@ -1,14 +1,21 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
 import geopandas as gpd
 from PIL import Image
+import warnings
+
+# Ignore all warnings
+warnings.filterwarnings("ignore")
 '''
 https://www.naturalearthdata.com/downloads/110m-cultural-vectors/
 
 https://jan-46106.medium.com/plotting-maps-with-european-data-in-python-part-i-decd83837de4
 '''
+
+OUTPUT_PATH = './output/'
 
 
 # load the low resolution world map
@@ -61,7 +68,12 @@ def plot_map_world(extra_data: pd.DataFrame = None, area: str = None):
     # plt.show()
 
 
-def plot_map_europe(extra_data: pd.DataFrame = None, cmap=None):
+def plot_map_europe(
+        extra_data: pd.DataFrame = None, 
+        cmap=None, 
+        bound=None,
+        title: str = None
+    ):
     '''
     You can set area to be
     1. No specify: no filtering
@@ -85,7 +97,8 @@ def plot_map_europe(extra_data: pd.DataFrame = None, cmap=None):
     # Include Cyprus (for current need) and exclude Russia (too large)
     countries = gpd.read_file('./data/ne_10m_admin_0_countries/')  # high res
 
-    cond = (countries['CONTINENT'] == "Europe") |\
+    cond = (countries['NAME'] != "Russia") &\
+        (countries['CONTINENT'] == "Europe") |\
         (countries['NAME'] == "Cyprus")
 
     countries = countries.loc[cond, cols_to_keep].reset_index(drop=True)
@@ -95,7 +108,12 @@ def plot_map_europe(extra_data: pd.DataFrame = None, cmap=None):
     countries.fillna({'color': '#808080'}, inplace=True)
 
     sizex, sizey = 12, 9
-    ax = countries.plot(figsize=(sizex, sizey), color=countries.color)
+    ax = countries.plot(
+        figsize=(sizex, sizey), 
+        color=countries.color,
+        edgecolor='black',        # Color of the edges
+        linewidth=0.5             # Width of the edges
+    )
 
     # actually, setting the display limit solves the territory problem
     ax.set_xlim([-25, 45])  # Longitude
@@ -103,27 +121,72 @@ def plot_map_europe(extra_data: pd.DataFrame = None, cmap=None):
 
     ax.set_aspect(1.5)  # adjust the display ratio
 
+    # ======================
+    # add the country labels
+    # ======================
+    # extra_data = extra_data.sort_values(
+    #     by='friendly_index_net', ascending=False, inplace=False
+    # )
+    # lengend_txt = []
+    # for i, c in enumerate(extra_data['NAME']):
+    #     cur_row = countries[countries['NAME'] == c]
+    #     sn = i + 1
+    #     lengend_txt.append(f"{sn}: {c}")
+    #     plt.text(
+    #         cur_row['geometry'].centroid.x,
+    #         cur_row['geometry'].centroid.y,
+    #         str(sn), ha='center', color='yellowgreen',
+    #         fontsize=8, weight='bold'
+    #     )
+    # lengend_txt = '\n'.join(lengend_txt)
+    # plt.text(
+    #     1.1, 0.5, lengend_txt, transform=ax.transAxes, 
+    #     fontsize=10, verticalalignment='center'
+    # )
+    # # Adjust layout to make room for the legend
+    # plt.subplots_adjust(right=0.8)
+
+    # =============
+    # add color bar
+    # =============
     sm = plt.cm.ScalarMappable(cmap=cmap)
     cbar = plt.colorbar(
         sm, ax=ax, fraction=0.035, pad=0.04,
-        orientation='horizontal'
+        orientation='vertical'
     )
-    ticks = [
-        'Very High', 'High', 'Neutral High',
-        'Neutral Low', 'Low', 'Very Low'
-    ]
-    cbar.set_ticklabels(ticks)
+    np_bound = np.array(bound)
+    normalized = (np_bound - min(np_bound)) / (max(np_bound) - min(np_bound))
+    cbar.set_ticks(normalized)
+    cbar.set_ticklabels(
+        np.round(bound, 0).astype(int), 
+        ha='right', # horizontal alignment
+        x=2.0 # the starting point of label, =0 will be overlapping with the colorbar
+    )
+
+    # ticks_lbs = [
+    #     'Very Low', 'Low', 'Neutral Low',
+    #     'Neutral High', 'High', 'Very High'
+    # ]
+    # cbar.set_ticklabels(ticks_lbs)
     cbar.ax.tick_params(labelsize=8)
+    cbar.ax.set_position([0.25, 0.2, 0.03, 0.38])  # (left, bottom, width, height)
+
     cbar.set_label('Friendly Index')
+    cbar.ax.yaxis.set_label_position('left')
+
+    if title:
+        plt.title(title, fontsize=16, weight='bold')
 
     plt.axis("off")
-    plt.show()
+    plt.savefig(OUTPUT_PATH + f'{title}.svg')
+    # plt.show()
 
 
 def generate_color_col(
     srs: pd.Series,
-    levels: int,
-    colors: list = ['blue', 'white', 'red']
+    levels: int = None,
+    step: int = 1,
+    colors: list = ['red', 'white', 'blue']
 ):
     """
     Generates a custom blue-to-red colormap with the specified number of levels.
@@ -134,11 +197,19 @@ def generate_color_col(
     Returns:
         LinearSegmentedColormap: The generated colormap.
     """
-    cmap = mcolors.LinearSegmentedColormap.from_list(
-        "custom_color", colors, N=levels)
+    mv = max(abs(srs))
+    if not levels and step:
+        t = 2 if srs.lt(0).any() else 1
+        levels = np.ceil(mv / step) * t
 
-    norm = mcolors.Normalize(vmin=0, vmax=100)
-    return [mcolors.to_hex(cmap(norm(p))) for p in srs], cmap
+    levels = int(levels)
+    bound = np.linspace(-mv, mv, levels+1)
+
+    cmap = mcolors.LinearSegmentedColormap.from_list(
+        "custom_color", colors, N=levels
+    )
+    norm = mcolors.Normalize(vmin=-mv, vmax=mv)
+    return [mcolors.to_hex(cmap(norm(p))) for p in srs], cmap, bound
 
 
 def task_europe_friendliness():
@@ -151,36 +222,40 @@ def task_europe_friendliness():
     df = df[cols_to_keep]
     df['year'] = df['year'].astype('int32')
 
-    # df.to_csv(
-    #     "./data/eu_country_name_mapping.csv",
-    #     index=False, encoding='utf-8-sig'
-    # )
-    # return
-
     name_map = pd.read_csv("./data/eu_country_name_mapping.csv")
 
     df = pd.merge(df, name_map, on='country', how='left')
 
-    df['color'], cmap = generate_color_col(
-        srs=df['friendly_index_net'].rank(pct=True) * 100,
-        levels=10
+    df['color'], cmap, bound = generate_color_col(
+        srs=df['friendly_index_net'],
+        step=3
     )
 
-    cur_year = 2017
+    y_list = sorted(set(df['year']))
+    # y_list = [2017]
 
-    # not all countries are included in each yaer
-    df = df.loc[df['year'] == cur_year, ['NAME', 'color']]
-    print(df.columns)
-    plot_map_europe(df, cmap)
-    # 全部加總的顏色要另外等加總完之後再做
+    for cur_year in y_list:
+        # not all countries are included in each yaer
+        cols = ['NAME', 'friendly_index_net', 'color']
+        tmp = df.loc[df['year'] == cur_year, cols]
+        plot_map_europe(tmp, cmap, bound, title=f"{cur_year}")
+
+    # ==========
+    # Total Year
+    # ==========
+    grouped_df = df.groupby('NAME')['friendly_index_net'].sum().reset_index()
+
+    grouped_df['color'], cmap, bound = generate_color_col(
+        srs=grouped_df['friendly_index_net'],
+        step=3
+    )
+    plot_map_europe(grouped_df, cmap, bound, title="2017-2023")
 
     return
 
 
 def main():
     task_europe_friendliness()
-    # plot_map_europe()
-    # plot_map_world(area="Europe")
 
 
 if __name__ == "__main__":
